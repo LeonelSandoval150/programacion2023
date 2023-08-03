7d78f473669bb05cb18d8e07e3e9e1a6c7b5be32
LeonelSandoval150 <leonelmaximilianosandoval@gmail.com>
2023-08-03 11:48:00 -0300

    primer commit

---
+++ src/app/models/modelos.ts
@@ -0,0 +1,15 @@
+export interface TarjetasInicio {
+    titulo: string;
+    descripcion:string;
+    imagen:string;
+    alt:string;
+}
+
+export interface TarjetasNosotros {
+    titulo: string;
+    descripcion:string;
+    imagen:string;
+    alt:string;
+}
+
+
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {AppRoutingModule} from '../app-routing.module';
+//material
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
+
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    AppRoutingModule,
+    MatButtonModule
+  ],
+  exports :[
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ]
+
+})
+export class SharedModule { }
